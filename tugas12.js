b63e95f0bc0da9de5f5b6650e6e7ac935eaa349a
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2019-08-19 22:13:31 +0800

    Tugas 12 Java Script
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+function namabuah(){
+  var buah = ['Pisang', 'Jeruk','Apel','Mangga']
+  console.log(buah);
+  buah.pop()
+  console.log(buah);
+  buah.shift()
+  return buah
+
+}
+console.log(namabuah());
